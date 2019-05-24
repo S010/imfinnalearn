749a72b9cb93427246c206bcbee35df9eb8d78a7
charlie <cgrimshawdev@gmail.com>
2019-05-24 13:46:30 +0100

    fixed csv import

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as t
 import datetime
 import sqlite3 as s
 from sqlite3 import Error
-import hashlib, binascii , os ,random ,CSV
+import hashlib, binascii , os ,random ,csv
 #https://stackabuse.com/a-sqlite-tutorial-with-python/
 con = s.connect("dbFile.db")
 cur = con.cursor()
@@ -71,7 +71,7 @@ def checkForKeyCode(r,table):
         print("key already exists")
         return True
 
-<<<<<<< Updated upstream
+
 def checkForUID(uid):
     cur.execute('SELECT uid FROM items WHERE uid='+str(uid))
     data = cur.fetchall()
@@ -86,8 +86,8 @@ def checkForUID(uid):
 
 
 #???? print()? *Jacob - I dont know how to do this or what the file format is gonna look like so i just gave up
-=======
->>>>>>> Stashed changes
+
+
 def getKeyCode(di):
     with open(di,"r") as data:
         reader = csv.reader(data)
@@ -97,7 +97,7 @@ def getKeyCode(di):
 def generateKeyCode():
     while True:
         randomKey = random.randint(0,999999999)
-<<<<<<< Updated upstream
+
         print("creates key")
         if checkForKeyCode(randomKey,"users") == False:
             print("if statement")
@@ -106,22 +106,22 @@ def generateKeyCode():
 
 #creates a salted hash of p, why the swap from sha256 for the salt to
 #pbkdf2_hmac for the hash???
-=======
+
         if checkForKeyCode(randomKey,"users") == False and checkForKeyCode(randomKey,"admins") == False:
             return randomKey
 
->>>>>>> Stashed changes
+
 def hashingPassword(p):
     salt = hashlib.sha256(os.urandom(60)).hexdigest().encode("ascii")
     passwordHash = hashlib.pbkdf2_hmac("sha256",p.encode("utf-8"),salt,100000)
     passwordHash = binascii.hexlify(passwordHash)
     return(salt+passwordHash).decode("ascii")
 
-<<<<<<< Updated upstream
+
 #s = hashedPassword from hashingPasword , p = password to check against/password
 #This function checks if a hashedpassword is the same as a string, returns a boolean value
-=======
->>>>>>> Stashed changes
+
+
 def verifyPassword(s,p):
     salt = s[:64]
     s = s[64:]
@@ -131,7 +131,7 @@ def verifyPassword(s,p):
 
 
 class User:
-<<<<<<< Updated upstream
+
     #initializes to create variables for name, keycode and id
     def __init__(self,firstname,lastname,keycode,userType,password):
         self.firstname = firstname
@@ -140,14 +140,14 @@ class User:
         self.userType = userType
         self.userType = password
         self.name = self.firstname+" "+self.lastname
-=======
+
     def __init__(self):
         self.firstname = ""
         self.lastname = ""
         self.keycode = ""
         self.id = ""
 
->>>>>>> Stashed changes
+
     def takeItemOut(self):
         pass
     def takeItemsBack(self):
@@ -157,32 +157,32 @@ class User:
     def getDetails(self):
         pass
 class Student(User):
-<<<<<<< Updated upstream
+
     def UI(self):
         print("student ui")
 #teacher class, inherits User.
-=======
+
     def __init__(self):
         pass
 
->>>>>>> Stashed changes
+
 class Teacher(User):
     def UI(self):
         print("teacher ui")
 
 class Admin(User):
-<<<<<<< Updated upstream
+
     def __init__(self,password):
         self.password = password
-=======
+
     def __init__(self):
         self.password = ""
-        
+
     def adminUI(self):
         print("~~ WELCOME TO DONCASTER UNIVERSITY LIBRARY ~~")
         while True:
             command = input("1.)Take Out Item\n2.)Put Item Back\n3.)Search Items\n4.)")
->>>>>>> Stashed changes
+
 
     def addItem(self):
         uid = input("Enter UID:")
@@ -204,17 +204,17 @@ class Admin(User):
                 print("Item deleted")
                 break
             else:
-<<<<<<< Updated upstream
+
                 print("||Invalid Input||")
 
     #creates a user object using a boolean for staff and admin and an integer
     #for pin its pin ,WHEN PROGRAMMING FOR USER MAKE PIN "" since the user has no password
     def createUser(userType,pin):
-=======
+
                 print("||That is not a command||")
-                
+
     def createUser(staff,admin,pin):
->>>>>>> Stashed changes
+
         keyCode = generateKeyCode()
         firstName = input("First Name:")
         lastName = input("Last Name:")
@@ -222,21 +222,21 @@ class Admin(User):
         con.commit()
         print("User {} {} {} Created".format(keyCode,firstName,lastName))
 
-<<<<<<< Updated upstream
+
     #returns all data in a specific table.
-=======
+
     def createStudent():
         createUser(False,False,"")
-        
+
     def createTeacher():
         createUser(True,False,"")
-        
+
     def createAdmin():
         password = input("Password:")
         password =hashingPassword(password)
         createUser(False,True,password)
-        
->>>>>>> Stashed changes
+
+
     def seeAllAccounts(table):
         cur.execute('SELECT * FROM '+table)
         data = cur.fetchall()
@@ -254,10 +254,10 @@ class Admin(User):
                 break
             else:
                 print("||Invalid Input||")
-        
+
     def seeRecentLogs(self):
         pass
-    
+
 
     #right now this asks for a command and if it is 1, it says "||Scans can not
     #be produced at the moment||" ? pretty sick tbh , will add nore when more is added
@@ -293,7 +293,7 @@ class Admin(User):
             else:
                 print("||That is not a command||")
 
-<<<<<<< Updated upstream
+
     #basic ui for the admin
     def UI(self):
         print("~~ WELCOME TO DONCASTER UNIVERSITY LIBRARY ~~")
@@ -315,9 +315,9 @@ class Admin(User):
 
 #Login function to log into the system, currently just checks to see if
 #keycode is valid.
-=======
+
 #Login function to log into the system
->>>>>>> Stashed changes
+
 def login():
     while True:
         command = input("Select which method of sign in you would like -\n1.)Scan Card\n2.)Enter Key Code\nb.)Back\n>>").lower().strip(" ")
@@ -331,14 +331,13 @@ def login():
                 cur.execute("SELECT keycode FROM users WHERE userType=0 AND WHERE keycode=",keycode)
             else:
                 print("||Key Code doesnt Exist||")
-<<<<<<< Updated upstream
-=======
-            
+
+
+
         elif command == "3":
             keyCode = input("Enter Key Code:")
             if checkForKeyCode(keyCode,"admins") == True:
                 pass
->>>>>>> Stashed changes
         elif command == "b":
             break
         else :
